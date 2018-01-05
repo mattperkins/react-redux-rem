04d3469d9fa8beab507ab5dc0245fb10067af172
mattperkins <github@mattperkins.com>
2018-01-05 19:20:12 +0000

    add server/index.html

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
 app.get("/*", (req, res) => {
-  res.send("Hello, World!");
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 app.listen(3100, () => console.log("Running on localhost:3100"));
