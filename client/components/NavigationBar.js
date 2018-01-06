11105e5f205b3515aaecac006c278f13ffbc3cc0
mattperkins <github@mattperkins.com>
2018-01-06 14:35:01 +0000

    add react router and component pages

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,19 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default () => {
   return (
     <nav className="navbar navbar-default">
       <div className="container-fluid">
         <div className="navbar-header">
-          <a className="navbar-brand" href="#">
+          <Link to="/" className="navbar-brand">
             DORMshed
-          </a>
+          </Link>
         </div>
 
         <div className="collapse navbar-collapse">
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#">Sign up</a>
+              <Link to="signup">Sign up</Link>
             </li>
           </ul>
         </div>
--- client/index.js
+++ client/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./components/app";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-render(<App />, document.getElementById("app"));
+import App from "./components/App";
+import Greetings from "./components/Greetings";
+import SignupPage from "./components/SignupPage";
+
+render(
+  <Router>
+    <App>
+      <Switch>
+        <Route exact path="/" component={Greetings} />
+        <Route path="/signup" component={SignupPage} />
+      </Switch>
+    </App>
+  </Router>,
+  document.getElementById("app")
+);
