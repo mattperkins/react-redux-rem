0a9ba7af6c6273a1214ecd918edbe6650fe853bd
mattperkins <github@mattperkins.com>
2018-01-07 20:02:12 +0000

    add flash message reducer

--- client/components/SignupPage.js
+++ client/components/SignupPage.js
@@ -3,15 +3,16 @@ import SignupForm from "./SignupForm";
 import { connect } from "react-redux";
 import { userSignupRequest } from "../actions/signupActions";
 import PropTypes from "prop-types";
-
+import { addFlashMessage } from "../actions/flashMessages";
 class SignupPage extends React.Component {
   render() {
-    const { userSignupRequest } = this.props;
+    const { userSignupRequest, addFlashMessage } = this.props;
     return (
       <div className="row">
         <div className="col-md-4 col-md-offset-4">
           <SignupForm
             userSignupRequest={userSignupRequest}
+            addFlashMessage={addFlashMessage}
             history={this.props.history}
           />
         </div>
@@ -22,6 +23,9 @@ class SignupPage extends React.Component {
 
 SignupPage.propTypes = {
   userSignupRequest: PropTypes.func.isRequired,
+  addFlashMessage: PropTypes.func.isRequired,
   history: PropTypes.object.isRequired
 };
-export default connect(null, { userSignupRequest })(SignupPage);
+export default connect(null, { userSignupRequest, addFlashMessage })(
+  SignupPage
+);
